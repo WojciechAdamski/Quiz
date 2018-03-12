b14543389da858b09ac0dc398053c5fe3ff6800f
WojciechAdamski <32711225+WojciechAdamski@users.noreply.github.com>
2018-03-12 20:12:03 +0100

    Add files via upload

---
+++ wersja3/quiz tablica/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace quiz_tablica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(503, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Wylosuj pytanie";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(270, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "C";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(270, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 65);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "D";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(443, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(71, 29);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(324, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Punktacja:";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(11, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 65);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "A";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(11, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(244, 65);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(265, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Czas na pytanie:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(443, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(71, 29);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(584, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(243, 31);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Imie";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(584, 100);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(243, 35);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Tablica wynikow";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(718, 219);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(71, 29);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(612, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Czas gry:";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(584, 49);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(243, 45);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "START";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(584, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Czyszczenie tablicy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(842, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Quiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ wersja3/quiz tablica/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace quiz_tablica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int pkt;
+        int x;
+        int z;
+        int time = 6;
+        int time1 = 51;
+        int[] tablica_pytania = new int[25];
+        int i = 1;
+        Random r = new Random();
+
+
+
+
+        void zerowanie()
+        {
+            pkt = 0;
+            textBox1.Text = "Wylosuj pytanie";
+            button4.Text = "A";
+            button5.Text = "B";
+            button2.Text = "C";
+            button3.Text = "D";
+            time = 6;
+            textBox2.Text = pkt.ToString();
+        }
+
+
+        void losowanie()
+        {
+            while (tablica_pytania.Contains(x))
+
+            {
+                x = r.Next(1, 20);
+            }
+
+
+            tablica_pytania[i] = x;
+            i++;
+
+
+            if (i == 20)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                Array.Clear(tablica_pytania, 0, tablica_pytania.Length);
+                if (pkt > 9) { File.AppendAllText(@"wyniki.txt", pkt + "pkt.    " + "    Zostalo czasu : " + time1 + "s.       " + textBox4.Text + Environment.NewLine); }
+                MessageBox.Show("Brawo. Udzielono odpowiedzi na wszystkie pytania!  Wynik:  " + pkt + " pkt");
+                zerowanie();
+                button();
+                i = 1;
+            }
+
+            else
+            {
+                z = x * 5;
+
+                textBox1.Text = File.ReadLines(@"pytania.txt").Skip(z).Take(1).First();
+                button4.Text = File.ReadLines(@"pytania.txt").Skip(z + 1).Take(1).First();
+                button5.Text = File.ReadLines(@"pytania.txt").Skip(z + 2).Take(1).First();
+                button2.Text = File.ReadLines(@"pytania.txt").Skip(z + 3).Take(1).First();
+                button3.Text = File.ReadLines(@"pytania.txt").Skip(z + 4).Take(1).First();
+
+
+                timer1.Start();
+                textBox2.Text = pkt.ToString();
+            }
+        }
+
+
+        void button ()
+        {
+            button7.Enabled = true; //START
+            button4.Enabled = false; //A
+            button5.Enabled = false; //B
+            button2.Enabled = false; //C
+            button3.Enabled = false; //D
+            textBox4.Enabled = true;
+        }
+
+
+        void timer()
+        {
+            timer1.Stop();
+            timer2.Stop();
+            if (pkt > 9) { File.AppendAllText(@"wyniki.txt", pkt + "pkt.    " + "    Zostalo czasu : " + time1 + "s.       " + textBox4.Text + Environment.NewLine); }
+            MessageBox.Show("Koniec czasu!  Wynik:  " + pkt + " pkt");
+            zerowanie();
+            button();
+            i = 1;
+        }
+
+
+        void zla_odp()
+        {
+            timer1.Stop();
+            timer2.Stop();
+            if (pkt > 9) { File.AppendAllText(@"wyniki.txt", pkt + "pkt.    " + "    Zostalo czasu : " + time1 + "s.       " + textBox4.Text + Environment.NewLine); }
+            MessageBox.Show("Zla odpowiedz.  Wynik:  " + pkt + " pkt");
+            zerowanie();
+            button();
+            i = 1;
+        }
+
+
+        private void button7_Click(object sender, EventArgs e) // START
+        {
+            time = 6;
+            time1 = 51;
+            pkt = 0;
+            timer2.Start();
+            losowanie();
+            button7.Enabled = false; //START
+            button4.Enabled = true; //A
+            button5.Enabled = true; //B
+            button2.Enabled = true; //C
+            button3.Enabled = true; //D
+            textBox4.Enabled = false;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)   //A
+        {
+            if (z <= 20)
+            {
+                time = 6;
+                pkt = pkt + 1;
+                losowanie();
+            }
+            else
+            {
+                zla_odp();
+            }
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)  //B
+        {
+            if (z > 20 & z <= 40)
+            {
+                time = 6;
+                pkt = pkt + 1;
+                losowanie();
+            }
+            else
+            {
+                zla_odp();
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)  // C
+        {
+            if (z > 40 & z <= 60)
+            {
+                time = 6;
+                pkt = pkt + 1;
+                losowanie();
+            }
+            else
+            {
+                zla_odp();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) // D
+        {
+            if (z > 60 & z <= 100)
+            {
+                time = 6;
+                pkt = pkt + 1;
+                losowanie();
+            }
+            else
+            {
+                zla_odp();
+            }
+        }
+
+
+        private void button6_Click(object sender, EventArgs e) // Tablica wynikow
+        {
+            string[] readText = File.ReadAllLines(@"wyniki.txt");
+            Array.Sort(readText);
+            Array.Reverse(readText);
+            MessageBox.Show("1 miejsce:   " + readText[0] + Environment.NewLine + "2 miejsce:   " + readText[1] + Environment.NewLine + "3 miejsce:   " + readText[2] + Environment.NewLine + "4 miejsce:   " + readText[3] + Environment.NewLine + "5 miejsce:   " + readText[4]);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)  // Czyszczenie tablicy
+        {
+            if (MessageBox.Show("Czy jestes pewien?", "Potwierdzenie", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                MessageBox.Show("Wyniki zostaly skasowane");
+                File.WriteAllText(@"wyniki.txt", " " + Environment.NewLine + " " + Environment.NewLine + " " + Environment.NewLine + " " + Environment.NewLine + " " + Environment.NewLine);
+            }
+            else
+            {
+                MessageBox.Show("Wyniki nie zostaly skasowane");
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time--;
+            textBox3.Text = time.ToString();
+            if (time == 0)
+            {
+                timer();
+            }
+        }
+             
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time1--;
+            textBox5.Text = time1.ToString();
+            if (time1 == 0)
+            {
+                timer();
+            }
+        }
+    }
+}
+
